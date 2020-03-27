1c993df592d8571188edd47d952e249eac75ebfb
Leviathan <zhu.quntao@foxmail.com>
2020-03-27 22:08:55 +0800

    初次规范测试目录，并上传空单元测试

---
+++ src/test/java/TestWayAndWinTest.java
@@ -0,0 +1,26 @@
+package test.java;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestWayAndWinTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testWay() {
+    }
+
+    @Test
+    public void testWin() {
+    }
+}
