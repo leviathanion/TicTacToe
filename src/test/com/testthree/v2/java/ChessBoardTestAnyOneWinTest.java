ec5f7508e1c0ae83ce0b896273284d6a97f76ef3
Leviathan <zhu.quntao@foxmail.com>
2020-04-02 21:52:42 +0800

    对ChessBoard的testAnyOneWin方法进行单元测试

---
+++ src/test/com/testthree/v2/java/ChessBoardTestAnyOneWinTest.java
@@ -0,0 +1,60 @@
+package com.testthree.v2.java;
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ChessBoardTestAnyOneWinTest {
+    static ChessBoard cb;
+    private boolean expected;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        cb = new ChessBoard();
+    }
+
+    public ChessBoardTestAnyOneWinTest(int r, int c, int[][] mat, boolean expected) {
+        cb = new ChessBoard();
+        cb.setRow(r);
+        cb.setCol(c);
+        cb.setMat(mat);
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters(name = "{index}:testWin[{0},{1}]={3}")
+    public static Collection testDate(){
+        return Arrays.asList(new Object[][]{
+                //横向
+                {1,0,new int[][]{{0,0,0},{0,0,0},{0,0,0}},false},
+                {1,0,new int[][]{{0,0,0},{1,1,1},{0,0,0}},true},
+                {1,0,new int[][]{{0,0,0},{2,2,2},{0,0,0}},true},
+                //纵向
+                {2,1,new int[][]{{0,0,0},{0,0,0},{0,0,0}},false},
+                {2,1,new int[][]{{0,1,0},{0,1,0},{0,1,0}},true},
+                {2,1,new int[][]{{0,2,0},{0,2,0},{0,2,0}},true},
+                //对角线
+                {2,2,new int[][]{{0,0,0},{0,0,0},{0,0,0}},false},
+                {2,2,new int[][]{{1,0,0},{0,1,0},{0,0,1}},true},
+                {2,2,new int[][]{{2,0,0},{0,2,0},{0,0,2}},true},
+        });
+    }
+
+    @Test
+    public void testAnyOneWin() {
+        assertEquals(expected, cb.testAnyOneWin());
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        cb = null;
+    }
+}
