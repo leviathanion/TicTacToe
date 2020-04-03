12ab3562f0fa71e258c26ea083981600bfeacd05
J980419xq <1908393204@qq.com>
2020-04-03 08:35:46 +0800

    增加测试用例

--- src/test/com/testthree/v1/demo/java/PrintMatTest.java
+++ src/test/com/testthree/v1/demo/java/PrintMatTest.java
@@ -1,16 +1,43 @@
 package com.testthree.v1.demo.java;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 import java.io.ByteArrayOutputStream; // 输出缓冲区
 import java.io.PrintStream; // 打印输出流
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
-
+@RunWith(Parameterized.class)
 public class PrintMatTest {
     PrintStream console = null;
     ByteArrayOutputStream bytes = null;
     PrintMat printMat;
-    public static  int [][] mat={{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    private String expected;
+
+    public  PrintMatTest(int [][] mat,String expected){
+        this.expected = expected;
+        GameDriver.mat = mat;
+    }
+
+    @Parameterized.Parameters/**构造测试用例*/
+    public static Collection data(){
+        return Arrays.asList(new Object[][]{
+                {new int[][]{{0,0,0},{0,0,0},{0,0,0}},"  1 2 3"+"\n1 . . . "+"\n2 . . . "+"\n3 . . . "},
+                {new int[][]{{1,0,0},{0,0,0},{0,0,0}},"  1 2 3"+"\n1 x . . "+"\n2 . . . "+"\n3 . . . "},
+                {new int[][]{{1,2,0},{0,0,0},{0,0,0}},"  1 2 3"+"\n1 x O . "+"\n2 . . . "+"\n3 . . . "},
+                {new int[][]{{1,2,1},{0,0,0},{0,0,0}},"  1 2 3"+"\n1 x O x "+"\n2 . . . "+"\n3 . . . "},
+                {new int[][]{{1,2,1},{2,0,0},{0,0,0}},"  1 2 3"+"\n1 x O x "+"\n2 O . . "+"\n3 . . . "},
+                {new int[][]{{1,2,1},{2,1,0},{0,0,0}},"  1 2 3"+"\n1 x O x "+"\n2 O x . "+"\n3 . . . "},
+                {new int[][]{{1,2,1},{2,1,2},{0,0,0}},"  1 2 3"+"\n1 x O x "+"\n2 O x O "+"\n3 . . . "},
+                {new int[][]{{1,2,1},{2,1,2},{1,0,0}},"  1 2 3"+"\n1 x O x "+"\n2 O x O "+"\n3 x . . "},
+                {new int[][]{{1,2,1},{2,1,2},{1,2,0}},"  1 2 3"+"\n1 x O x "+"\n2 O x O "+"\n3 x O . "},
+                {new int[][]{{1,2,1},{2,1,2},{1,2,1}},"  1 2 3"+"\n1 x O x "+"\n2 O x O "+"\n3 x O x "},
+                });
+    }
 
     @org.junit.Before              // 预处理
     public void setUp() throws Exception {
@@ -32,11 +59,10 @@ public class PrintMatTest {
 
 
     @Test
-    public void printMatTest() throws Exception {
-        GameDriver.mat = mat;
+    public void printMat() throws Exception {
         printMat.printMat();
-        String s =new String("  1 2 3"+"\n1 . . . "+"\n2 . . . "+"\n3 . . . ");
-        assertEquals(s, bytes.toString());
+        //String s =new String("  1 2 3"+"\n1 . . . "+"\n2 . . . "+"\n3 . . . ");
+        assertEquals(this.expected, bytes.toString());
 
     }
 }
